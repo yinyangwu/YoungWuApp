658bffdd398f0e2158162f0690e9be704bb49739
yinyangwu <YoungWu@>
2020-06-29 13:49:59 +0800

    修改类型

--- app/src/main/java/com/youngwu/kotlin/myapplication/application/MyApp.kt
+++ app/src/main/java/com/youngwu/kotlin/myapplication/application/MyApp.kt
@@ -10,7 +10,7 @@ import android.app.Application
 class MyApp : Application() {
 
     companion object {
-        lateinit var app: MyApp
+        lateinit var app: Application
 
         fun getContext(): Application {
             return app
